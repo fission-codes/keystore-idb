85b63872137297d8d24596b67c3ad3f5a8eefbca
Daniel Holmgren <dtholmgren@gmail.com>
2020-02-18 22:58:29 -0600

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,108 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
+import * as idb from 'idb'
+import Store from './store'
 
-console.log('Hello world!')
+const DEFAULT_EC_CURVE = 'P-256'
+const DEFAULT_SYMM_ALG = 'AES-CTR'
+const READ_KEY = 'ecdh-key'
+const WRITE_KEY = 'ecdsa-key'
+
+async function encrypt(msg: string, publicKey: CryptoKey) {
+  const { privateKey } = await getReadKey()
+  const cipherKey = await crypto.subtle.deriveKey(
+    { name: 'ECDH', public: publicKey },
+    privateKey,
+    { name: DEFAULT_SYMM_ALG, length: 256 },
+    false,
+    ['encrypt', 'decrypt']
+  )
+  // window.crypto.subtle.encrypt(
+  //   { name: DEFAULT_SYMM_ALG,
+  //     counter: new Uint8Array(8),
+  //     length: 128
+  //   },
+  //   cipherKey,
+  //   msg
+  // )
+  // console.log(cipherKey)
+}
+
+async function getPublicReadKey() {
+  const keypair = await getReadKey()
+  const buffer = await crypto.subtle.exportKey("raw", keypair.publicKey)
+  return buffer
+}
+
+async function getReadKey() {
+  let keypair = await Store.getKey(READ_KEY)
+  if(!keypair) {
+    console.log('creating read key')
+    keypair = await makeReadKey()
+    await Store.putKey(READ_KEY, keypair)
+  }
+  return keypair
+}
+
+async function getWriteKey() {
+  let keypair = await Store.getKey(WRITE_KEY)
+  if(!keypair) {
+    console.log('creating write key')
+    keypair = await makeWriteKey()
+    await Store.putKey(WRITE_KEY, keypair)
+  }
+  return keypair
+}
+
+async function makeReadKey(): Promise<CryptoKeyPair> {
+  return crypto.subtle.generateKey(
+    { name: "ECDH", namedCurve: DEFAULT_EC_CURVE },
+    false, 
+    ['deriveKey', 'deriveBits']
+  ) 
+}
+
+async function makeWriteKey(): Promise<CryptoKeyPair> {
+  return crypto.subtle.generateKey(
+    { name: "ECDSA", namedCurve: DEFAULT_EC_CURVE },
+    false, 
+    ['sign', 'verify']
+  ) 
+}
+
+async function makeRSAKey(): Promise<CryptoKeyPair> {
+  return crypto.subtle.generateKey(
+    {
+        name: "RSA-OAEP",
+        modulusLength: 2048, //can be 1024, 2048, or 4096
+        publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+        hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+    },
+    false, //whether the key is extractable (i.e. can be used in exportKey)
+    ["encrypt", "decrypt"] //must be ["encrypt", "decrypt"] or ["wrapKey", "unwrapKey"]
+  ) 
+}
+
+function structuralClone(obj: any) {
+  return new Promise(resolve => {
+    const {port1, port2} = new MessageChannel();
+    port2.onmessage = ev => resolve(ev.data);
+    port1.postMessage(obj);
+  });
+}
+
+function arrbuffToString(arr: ArrayBuffer) {
+  const view = new Uint8Array(arr)
+  let result = ''
+  view.forEach(x => {
+    result += x.toString(16)
+  })
+  console.log(result)
+  return result
+}
+
+async function run() {
+}
+
+run()
+
 
 export const test = true
---
+++ src/store.ts
@@ -0,0 +1,67 @@
+type ModifyStoreFN = (store: IDBObjectStore) => void
+
+export async function putKey(id: string, keypair: CryptoKeyPair) {
+  return new Promise((resolve, reject) => {
+		callOnStore((store) => {
+			try{
+				const putData = store.put({ id, keypair })
+				putData.onsuccess = resolve
+				putData.onerror = reject
+			}catch(err) {
+				console.log("ERROR: ")
+				console.log(err)
+			}
+		})
+	})
+}
+
+export async function getKey(id: string): Promise<CryptoKeyPair | undefined> {
+  return new Promise((resolve, reject) => {
+    callOnStore((store) => {
+      const getData = store.get(id)
+      getData.onsuccess = async function () {
+        if(getData.result && getData.result.keypair){
+          return resolve(getData.result.keypair)
+        }else {
+          return resolve(undefined)
+        }
+      }
+      getData.onerror = reject
+    })
+  })
+}
+
+// from https://gist.github.com/saulshanabrook/b74984677bccd08b028b30d9968623f5
+export async function callOnStore(fn_: ModifyStoreFN) {
+	const windowIDB = window as any
+	// This works on all devices/browsers, and uses IndexedDBShim as a final fallback 
+	var indexedDB = windowIDB.indexedDB || windowIDB.mozIndexedDB || windowIDB.webkitIndexedDB || windowIDB.msIndexedDB || windowIDB.shimIndexedDB;
+
+	// Open (or create) the database
+	var open = indexedDB.open("MyDatabase", 1);
+
+	// Create the schema
+	open.onupgradeneeded = function() {
+	    var db = open.result;
+	    var store = db.createObjectStore("MyObjectStore", {keyPath: "id"});
+	};
+
+	open.onsuccess = function() {
+	    // Start a new transaction
+	    var db = open.result;
+	    var tx = db.transaction("MyObjectStore", "readwrite");
+	    var store = tx.objectStore("MyObjectStore");
+
+	    fn_(store)
+
+	    // Close the db when the transaction is done
+	    tx.oncomplete = function() {
+	        db.close();
+	    };
+	}
+}
+
+export default {
+	putKey,
+	getKey,
+}

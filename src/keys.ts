5f2145bf175b1df089e5027448305371848499fc
Daniel Holmgren <dtholmgren@gmail.com>
2020-02-19 23:19:08 -0600

    cleanup

--- src/keys.ts
+++ src/keys.ts
@@ -53,19 +53,6 @@ async function keyPairToPublic(keypair: CryptoKeyPair): Promise<string> {
   return utils.arrBufToHex(buffer)
 }
 
-// async function makeRSAKey(): Promise<CryptoKeyPair> {
-//   return crypto.subtle.generateKey(
-//     {
-//         name: "RSA-OAEP",
-//         modulusLength: 2048, //can be 1024, 2048, or 4096
-//         publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
-//         hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
-//     },
-//     false, //whether the key is extractable (i.e. can be used in exportKey)
-//     ["encrypt", "decrypt"] //must be ["encrypt", "decrypt"] or ["wrapKey", "unwrapKey"]
-//   ) 
-// }
-
 export default {
   getReadKey,
   getWriteKey,

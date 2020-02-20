9d6ecf7345a56282ea2d876cffee45143d9681d6
Daniel Holmgren <dtholmgren@gmail.com>
2020-02-19 19:28:04 -0600

    sign & verify + byte operations

--- src/constants.ts
+++ src/constants.ts
@@ -2,10 +2,12 @@ export const DEFAULT_EC_CURVE = 'P-256'
 export const READ_KEY = 'ecdh-key'
 export const WRITE_KEY = 'ecdsa-key'
 export const DEFAULT_SYMM_ALG = 'AES-CTR'
+export const DEFAULT_HASH_ALG = 'SHA-256'
 
 export default {
   DEFAULT_EC_CURVE,
   READ_KEY,
   WRITE_KEY,
   DEFAULT_SYMM_ALG,
+  DEFAULT_HASH_ALG,
 }
--- src/operations.ts
+++ src/operations.ts
@@ -1,6 +1,37 @@
 import keys from './keys'
 import utils from './utils'
-import { DEFAULT_SYMM_ALG } from './constants'
+import { DEFAULT_SYMM_ALG, DEFAULT_HASH_ALG } from './constants'
+
+export async function sign(msg: string) {
+  const sigBytes = await signBytes(utils.strToArrBuf(msg))
+  return utils.arrBufToHex(sigBytes)
+}
+
+export async function signBytes(data: ArrayBuffer): Promise<ArrayBuffer> {
+  const { privateKey } = await keys.getWriteKey()
+  return window.crypto.subtle.sign(
+    { name: "ECDSA", hash: {name: DEFAULT_HASH_ALG}},
+    privateKey,
+    data
+  )
+}
+
+export async function verify(msg: string, sig: string, publicKey: PublicKey): Promise<boolean> {
+  return verifyBytes(
+    utils.strToArrBuf(msg),
+    utils.hexToArrBuf(sig),
+    publicKey
+  )
+}
+
+export async function verifyBytes(data: ArrayBuffer, sig: ArrayBuffer, publicKey: PublicKey): Promise<boolean> {
+  return window.crypto.subtle.verify(
+    { name: "ECDSA", hash: {name: DEFAULT_HASH_ALG}},
+    publicKey,
+    sig, 
+    data 
+  )
+}
 
 export async function getSharedKey(publicKey: PublicKey): Promise<SymmKey> {
   const { privateKey } = await keys.getReadKey()
@@ -13,7 +44,12 @@ export async function getSharedKey(publicKey: PublicKey): Promise<SymmKey> {
   )
 }
 
-export async function encrypt(msg: string, publicKey: PublicKey): Promise<CipherText> {
+export async function encrypt(msg: string, publicKey: PublicKey): Promise<string> {
+  const cipherText = await encryptBytes(utils.strToArrBuf(msg), publicKey)
+  return utils.arrBufToHex(cipherText)
+}
+
+export async function encryptBytes(data: ArrayBuffer, publicKey: PublicKey): Promise<CipherText> {
   const cipherKey = await getSharedKey(publicKey)
   return crypto.subtle.encrypt(
     { name: DEFAULT_SYMM_ALG,
@@ -21,11 +57,16 @@ export async function encrypt(msg: string, publicKey: PublicKey): Promise<Cipher
       length: 128
     },
     cipherKey,
-    utils.strToArrBuf(msg)
+    data
   )
 }
 
-export async function decrypt(cipherText: CipherText, publicKey: CryptoKey): Promise<string> {
+export async function decrypt(cipherText: string, publicKey: CryptoKey): Promise<string> {
+  const msgBytes = await decryptBytes(utils.hexToArrBuf(cipherText), publicKey)
+  return utils.arrBufToStr(msgBytes)
+}
+
+export async function decryptBytes(cipherText: CipherText, publicKey: CryptoKey): Promise<ArrayBuffer> {
   const cipherKey = await getSharedKey(publicKey)
   const msgBuff = await crypto.subtle.encrypt(
     { name: DEFAULT_SYMM_ALG,
@@ -35,11 +76,17 @@ export async function decrypt(cipherText: CipherText, publicKey: CryptoKey): Pro
     cipherKey,
     cipherText
   )
-  return utils.arrBufToStr(msgBuff)
+  return msgBuff
 }
 
 export default {
+  sign,
+  signBytes,
+  verify,
+  verifyBytes,
   encrypt,
+  encryptBytes,
   decrypt,
+  decryptBytes,
   getSharedKey,
 }

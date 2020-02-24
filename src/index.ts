0406291c2ba921fac0eb5ac2bbd87734f842757b
Daniel Holmgren <dtholmgren@gmail.com>
2020-02-24 16:57:19 -0600

    index.ts exports

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,10 @@
 import KeyStore from './keystore'
 
-async function run() {
-  const ALG = 'ecc'
-  await KeyStore.clear()
-  const ks1 = await KeyStore.init({ type: ALG, readKeyName: 'read-key-1', writeKeyName: 'write-key-1' })
-  const ks2 = await KeyStore.init({ type: ALG, readKeyName: 'read-key-2', writeKeyName: 'write-key-2' })
-
-  const msg = "Incididunt id ullamco et do."
-  const readKey1 = ks1.readKey
-  const readKey2 = ks2.readKey
-  const writeKey1 = ks1.writeKey
-
-  const sig = await ks1.sign(msg)
-  const valid = await ks2.verify(msg, sig, writeKey1.publicKey)
-  console.log('sig: ', sig)
-  console.log('valid: ', valid)
-
-  const cipher = await ks1.encrypt(msg, readKey2.publicKey)
-  const decipher = await ks2.decrypt(cipher, readKey1.publicKey)
-  console.log('cipher: ', cipher)
-  console.log('decipher: ', decipher)
-
-  // read keys are write keys are separate because of the Web Crypto API
-  const readKey = await ks1.publicReadKey()
-  const writeKey = await ks1.publicWriteKey()
-  console.log('readKey: ', readKey)
-  console.log('writeKey: ', writeKey)
-}
-
-run()
+export default KeyStore
+ 
+export { default as ecc }  from './ecc'
+export { default as rsa }  from './rsa'
+export { default as config }  from './config'
+export { default as constants }  from './constants'
+export { default as utils }  from './utils'
+export { default as idb }  from './idb'

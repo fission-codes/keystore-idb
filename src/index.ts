7ee014b1b603881450302756e53bc67d1f996dab
Daniel Holmgren <dtholmgren@gmail.com>
2020-02-28 14:07:57 -0600

    change exports

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,5 @@
 import { init, clear } from './keystore'
 
-export * from './keystore'
-export * as ecc from './ecc'
-export * as rsa from './rsa'
-export * as config from './config'
-export * as constants from './constants'
-export * as utils from './utils'
-export * as idb from './idb'
-export * as types from './types'
-
 import * as ecc from './ecc'
 import * as rsa from './rsa'
 import * as config from './config'

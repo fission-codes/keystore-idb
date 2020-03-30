df6b9fc7d578fb53cf936fc48ad2793df38110af
Daniel Holmgren <dtholmgren@gmail.com>
2020-03-30 14:16:55 -0500

    better error handling

--- src/errors.ts
+++ src/errors.ts
@@ -1,19 +1,21 @@
 import { KeyUse, CryptoSystem } from './types'
 
+export const NotKeyPair = new Error("Retrieved a symmetric key when an asymmetric keypair was expected. Please use a different key name.")
+export const NotKey = new Error("Retrieved an asymmetric keypair when an symmetric key was expected. Please use a different key name.")
 export const ECCNotEnabled = new Error("ECC is not enabled for this browser. Please use RSA instead.")
 export const UnsupportedCrypto = new Error("Cryptosystem not supported. Please use ECC or RSA")
 export const InvalidKeyUse = new Error("Invalid key use. Please use 'read' or 'write")
 
 export function checkIsKeyPair(keypair: any): CryptoKeyPair {
   if(keypair.privateKey === undefined){
-    throw new Error("asdf")
+    throw NotKeyPair
   }
   return keypair as CryptoKeyPair
 }
 
 export function checkIsKey(key: any): CryptoKey {
-  if(key.privatdeKey !== undefined){
-    throw new Error("asdf")
+  if(key.privateKey !== undefined || key.algorithm === undefined){
+    throw NotKey
   }
   return key
 }
@@ -34,8 +36,13 @@ function checkValid<T>(toCheck: T, opts: T[], error: Error): void {
 }
 
 export default {
+  NotKeyPair,
+  NotKey,
   ECCNotEnabled,
   UnsupportedCrypto,
   InvalidKeyUse,
+  checkIsKeyPair,
+  checkIsKey,
+  checkValidCryptoSystem,
   checkValidKeyUse,
 }
